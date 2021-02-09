947561f7bd979279aea7195f965a16c1c76795ae
saurabhvaish93 <57400294+saurabhvaish93@users.noreply.github.com>
2021-02-10 00:01:39 +0530

    added spring cloud config for git

--- src/main/java/com/srv/app/product/controller/ProductController.java
+++ src/main/java/com/srv/app/product/controller/ProductController.java
@@ -3,19 +3,28 @@ package com.srv.app.product.controller;
 
 import com.srv.app.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 @RequestMapping("/api/v1/")
 public class ProductController {
 
     private final ProductService productService;
 
-    public ResponseEntity<String> getAllProducts(){
-        return new ResponseEntity<>("Success",HttpStatus.OK);
+    @Value("${prop.value:null}")
+    private String fromProp;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllProducts(){
+        System.out.println(fromProp);
+        return new ResponseEntity<>(productService.getAllProducts(),HttpStatus.OK);
     }
 }
